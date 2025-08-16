a3a04ec05afd0a1310e996f3169923f54bd24f08
Fabricio-Nunes-Ernandes <fabricio.ernandes@sptech.school>
2025-08-16 16:48:48 -0300

    Atividade feita

--- src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
@@ -1,13 +1,29 @@
 package school.sptech.sprint1_nota1.ex1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioFacilController {
 
     @GetMapping("/ex-01/{palavra}")
-    public Boolean exercicioFacil(String palavra) {
-        return null;
+    public Boolean exercicioFacil(@PathVariable String palavra) {
+
+        palavra = palavra.replaceAll(" ", "").toLowerCase();
+
+        String palavraInvertida = "";
+        Integer quantidade = palavra.length();
+        for (int i = quantidade - 1; i >= 0; i--) {
+            palavraInvertida += palavra.charAt(i);
+        }
+
+        for (int i = 0; i < quantidade; i++) {
+            if (palavra.charAt(i) != palavraInvertida.charAt(i)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
@@ -8,7 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExercicioDificilController {
 
     @GetMapping("/ex-03/{n}")
-    public ExercicioDificilResponse exercicioDificil(int n) {
-        return null;
-    }
-}
+    public ExercicioDificilResponse exercicioDificil(@PathVariable int n) {
+
+
+        if (n <= 0) {
+            return new ExercicioDificilResponse(0, 0);
+
+        } else {
+
+            int anterior1 = 1;
+            int anterior2 = 1;
+            int total = 0;
+            int enesimo = 0;
+            for (int i = 1; i <= n; i++) {
+
+                enesimo = anterior1;
+                total += enesimo;
+
+                int atual = anterior1 + anterior2;
+                anterior1 = anterior2;
+                anterior2 = atual;
+            }
+
+            return new ExercicioDificilResponse(enesimo, total);
+        }
+} }
